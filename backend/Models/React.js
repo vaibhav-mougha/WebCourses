bf40db4821810a4ac78a08a80386e6afa73c0676
Vaibhav Kumar Mougha <tunu1994@gmail.com>
2023-01-27 18:10:53 +0530

    Added Backend

---
+++ backend/Models/React.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const reactSchema = mongoose.Schema({
+    topic: {type:String, required:true},
+    desc: {type:String, required:true},
+    video: {type:String, required:true},
+    img: {type:String, required:true},
+    category: {type:String, required:true}
+});
+
+const ReactModel = mongoose.model("react", reactSchema);
+
+module.exports = { ReactModel };
